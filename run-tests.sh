d5ccf7938356e35ee5f5ddc3346750bccab9eaa7
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-01-15 11:19:33 +0000

    Update UI run test to show tests

--- run-tests.sh
+++ run-tests.sh
@@ -3,9 +3,11 @@ DEFAULT_BROWSER=chrome
 BROWSER_TYPE=$1
 ENV=$2
 
-if [ -z "$BROWSER_TYPE" ]; then
+if [ -z "$BROWSER_TYPE" ]
+then
     echo "BROWSER_TYPE value not set, defaulting to $DEFAULT_BROWSER..."
+else
     echo "BROWSER_TYPE value set to $BROWSER_TYPE..."
 fi
 
-sbt clean -Dbrowser="${BROWSER_TYPE:=$DEFAULT_BROWSER}" -Denvironment="${ENV:=local}" "testOnly uk.gov.hmrc.test.ui.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER_TYPE:=$DEFAULT_BROWSER}" -Denvironment="${ENV:=local}" test testReport
