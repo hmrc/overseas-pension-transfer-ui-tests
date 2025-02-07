365ebd25e08616d48b76692cf0ce225a2f0a95ca
Mathew May <29102292+mghmay@users.noreply.github.com>
2025-02-07 11:11:01 +0000

    Working in local

---
+++ src/test/scala/uk/gov/hmrc/ui/pages/AuthPage.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ui.pages
+
+import org.openqa.selenium.By
+import uk.gov.hmrc.configuration.TestEnvironment
+
+object AuthPage extends BasePage {
+  val url = "http://localhost:9949/auth-login-stub/gg-sign-in"
+
+  def goTo(): Unit = {
+    get(url)
+  }
+
+  def authorise(): Unit ={
+    this.goTo()
+    sendKeys(By.id("redirectionUrl"), TestEnvironment.url("overseas-pension-transfer-frontend"))
+    click(By.id("submit-top"))
+  }
+}
